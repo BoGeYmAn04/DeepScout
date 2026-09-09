fb63909cce1e3274282a93b032f00d0e854666eb
Priyash <priyashpatel13@gmail.com>
2026-09-10 00:52:50 +0530

    Added the Agents

--- Server/app/agents/ReaderAgent.py
+++ Server/app/agents/ReaderAgent.py
@@ -1,16 +1,13 @@
 from langchain.agents import create_agent
 from app.llm.gemini import get_llm
-from app.tools.WebSearch import web_search
-from rich import print
+from app.tools.WebScrap import web_scrap
 
 llm = get_llm()
 
-def build_search_agent():
-    tools = [web_search]
+def build_reader_agent():
+    tools = [web_scrap]
     agent = create_agent(
         model=llm,
         tools=tools
     )
     return agent
-
- 
